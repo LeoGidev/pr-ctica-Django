e0365cad3f2ad00365e7a14cd9d22f4a244e2557
LeoGidev <leogimenez.dev@gmail.com>
2024-05-17 16:54:28 -0300

    se trabaja en login

--- mi_proyecto/mi_app/models.py
+++ mi_proyecto/mi_app/models.py
@@ -1,7 +1,20 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # Aquí puedes añadir campos adicionales si lo deseas
-    pass
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # Cambiar 'user_set' a 'custom_user_set'
+        blank=True,
+        help_text=('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        related_query_name='custom_user'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_set',  # Cambiar 'user_set' a 'custom_user_set'
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='custom_user'
+    )
+
 
